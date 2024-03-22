7c50b14408c7a2400028e9c19993a80272d11b3b
Jonáš Sirko <jonas.sirko@gmail.com>
2024-03-22 16:19:30 +0100

    readd and fix set_max_current service

--- custom_components/chuck_control/sensor.py
+++ custom_components/chuck_control/sensor.py
@@ -80,7 +80,15 @@ async def async_setup_entry(
         async_add_entities(to_add)
     else:
         _LOGGER.error(f"Cannot add {chargebox}")
-
+    platform = entity_platform.async_get_current_platform()
+    platform.async_register_entity_service(
+        "set_max_charging_current",
+        {
+            vol.Required("entity_id"): cv.entity_id,
+            vol.Required("max_charging_current"): cv.positive_int,
+        },
+        "set_max_charging_current",
+    )
 
 def get_friendly_name(
     cls,
